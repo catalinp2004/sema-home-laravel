1c14b630cb7c9f726cf5ad6ff9cbab39b20fade6
Catalin Postelnicu <catalinp2004@gmail.com>
2025-09-30 20:12:22 +0300

    added queue:work command to scheduled tasks to run ona single cron

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,9 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('digest:send')->everyTenMinutes()->description('Send error digests');
+
+// Process the queue every minute in a short-lived worker; ideal for shared hosting
+Schedule::command('queue:work --stop-when-empty --sleep=3 --tries=3')
+    ->everyMinute()
+    ->withoutOverlapping()
+    ->description('Process queued jobs');
